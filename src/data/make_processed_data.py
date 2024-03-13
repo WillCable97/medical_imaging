ee744beade3fbedfd9387826d2ccceee10cca323
WillCable97 <94039566+WillCable97@users.noreply.github.com>
2024-03-12 21:33:28 -0500

    Formal make processed data

--- src/data/make_processed_data.py
+++ src/data/make_processed_data.py
@@ -6,26 +6,79 @@ import pydicom as dicom
 import cv2
 
 
-data_path = "./data"
-data_path = os.path.abspath(data_path)
-file_path = os.path.join(data_path, "raw", "med_records")
+root_dir = "./"
+root_dir = os.path.abspath(root_dir)
+data_path = os.path.join(root_dir, "data")
+raw_data_path = os.path.join(data_path, "raw")
 processed_path = os.path.join(data_path, "processed")
 
-def extract_full_files_paths(raw_path: str, file_name: str, processed_path: str):
-    dframe = pd.read_csv(os.path.join(raw_path, file_name))
-    path_list = dframe['File Location'].to_list()
-    diagnostic_flag = dframe['3rd Party Analysis'].to_list()
+#Label file
+label_file = os.path.join(raw_data_path, "labels.csv")
+d_frame = pd.read_csv(label_file)
+d_frame["label"] = (d_frame["Normal"] + (2* d_frame["Cancer"]) 
+                    + (3* d_frame["Actionable"]) + (4* d_frame["Benign"])) - 1
 
-    diagnostic_flag = [0 if x=="NO" else 1 for x in diagnostic_flag]
-    path_list = [os.path.join(raw_path, x, "1-1.dcm") for x in path_list]
 
-    return path_list, diagnostic_flag
+def get_label_info(file_path: str) -> pd.DataFrame:
+    d_frame = pd.read_csv(file_path)
+    d_frame["label"] = (d_frame["Normal"] + (2* d_frame["Cancer"]) 
+                        + (3* d_frame["Actionable"]) + (4* d_frame["Benign"])) - 1
+    
+    ret_df = d_frame[["PatientID", "StudyUID", "label"]]
+    ret_df = ret_df.drop_duplicates()
 
+    return ret_df
+
+
+label_info = get_label_info(label_file)
+
+
+#fEATURE DEFINITIONS
 def _int64_feature(value):
   return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+
+def extract_file_meta_data(raw_path: str, file_name: str):
+    dframe = pd.read_csv(os.path.join(raw_path, file_name))
+    meta_data = dframe[["Subject ID", "Study Date", "File Location"]]
+    meta_data.rename(columns={"Subject ID" :"PatientID"}, inplace=True)   
+
+    meta_data = meta_data.copy()
+    meta_data.loc[:, 'file_path'] = meta_data.apply(lambda row: os.path.join(raw_path, os.path.normpath(row['File Location']), "1-1.dcm"), axis=1)
+    
+    return meta_data#[["Study Date", "PatientID", "file_path"]]
+    
+
+
+all_file_paths = extract_file_meta_data(raw_path=os.path.join(raw_data_path, "manifest-1617905855234")
+                                          , file_name="metadata.csv")
+
+
+def append_labe_data(input_df, label_df):
+    print(input_df.shape)
+    print(label_df.drop_duplicates().shape)
+
+    input_df = input_df.reset_index()
+    label_df = label_df.reset_index()
+
+    df_result = pd.merge(input_df, label_df, on = "PatientID", how='left')
+
+    df_result['StudyUID'] = df_result['StudyUID'].str.lower()
+    df_result['File Location'] = df_result['File Location'].str.lower()
+    df_result = df_result[df_result.apply(lambda row: row['StudyUID'].lower() in row['File Location'].lower() 
+                                          if pd.notna(row['StudyUID']) and pd.notna(row['File Location']) else False, axis=1)]
+    
+    ret_df = df_result[["PatientID", "StudyUID", "label", "file_path"]]
+
+    return ret_df
+
+
+test = append_labe_data(all_file_paths, label_info)
+print(test.shape)
 
 def single_image_loader(input_path: str) -> np.array:
     output_height = 256
@@ -36,7 +89,8 @@ def single_image_loader(input_path: str) -> np.array:
     pix_arr = [cv2.resize(slice_data, (output_width, output_height)) for slice_data in pix_arr]    
     np_img = np.array(pix_arr)
     np_img = np.transpose(np_img, (1, 2, 0))
-    return np_img 
+    return np_img
+
 
 def convert_to_record(image_data, label, file_name):
     rows, columns, depth = image_data.shape
@@ -55,49 +109,12 @@ def convert_to_record(image_data, label, file_name):
     writer.write(example.SerializeToString())
 
 
-path_list, label = extract_full_files_paths(file_path, "metadata.csv", processed_path)
 
+for index, row in test.iterrows():
+    path = row["file_path"]
+    label = row["label"]
 
-for i, path in enumerate(path_list):
-    img_data = single_image_loader(path)
-    convert_to_record(img_data, 1, os.path.join(processed_path, f"record{i}.tfrecords"))
+    image_data = single_image_loader(path)
 
-
-
-"""
-def parse_tfrecord_fn(example_proto):
-    feature_description = {
-        'height': tf.io.FixedLenFeature([], tf.int64),
-        'width': tf.io.FixedLenFeature([], tf.int64),
-        'depth': tf.io.FixedLenFeature([], tf.int64),
-        'label': tf.io.FixedLenFeature([], tf.int64),
-        'image_raw': tf.io.FixedLenFeature([], tf.string),
-    }
-
-    example = tf.io.parse_single_example(example_proto, feature_description)
-    
-    height = tf.cast(example['height'], tf.int32)
-    width = tf.cast(example['width'], tf.int32)
-    depth = tf.cast(example['depth'], tf.int32)
-    label = tf.cast(example['label'], tf.int32)
-
-    image_raw = tf.io.decode_raw(example['image_raw'], tf.int16)
-    image = tf.reshape(image_raw, (height, width, depth))
-
-    return image, label
-
-
-# Create a dataset from the TFRecord file
-dataset = tf.data.TFRecordDataset([os.path.join(processed_path, "test.tfrecords")])
-
-# Map the parse_tfrecord_fn function to each element in the dataset
-dataset = dataset.map(parse_tfrecord_fn)
-
-# Create an iterator and get the next element
-iterator = iter(dataset)
-image, label = iterator.get_next()
-
-
-print(label)
-print(img_data.reshape(-1).shape)
-"""
+    file_path = os.path.join(processed_path, f"record{index}.tfrecords")
+    convert_to_record(image_data, label, file_path) 
